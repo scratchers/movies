c5065060950b8d9223ada00c1c7069c15b46750e
Jeff Puckett <jeff@jeffpuckett.com>
2017-02-10 04:22:11 +0000

    stub basic views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,14 +36,14 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        {{ config('app.name', 'Movies') }}
                     </a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                        <li><a href="{{ route('movies.index') }}">Browse</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -53,6 +53,7 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                            <li><a href="{{ route('home') }}">Home</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::name('home')->get('/home', 'HomeController@index');
 
 Route::resource('movies', 'MovieController');
