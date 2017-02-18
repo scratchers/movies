b38f621fd37d6fad27fa478ad316b180657005a7
Jeff Puckett <jeff@jeffpuckett.com>
2017-02-18 19:53:43 +0000

    add movies view partial form

--- resources/views/movies/create.blade.php
+++ resources/views/movies/create.blade.php
@@ -5,31 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
 
-            <div class="panel panel-default">
-                <div class="panel-heading">{{ $movie->basename }}</div>
-                <div class="panel-body">
-                <form class="form-horizontal" role="form" method="POST" action="{{ route('movies.store') }}">
-                    {{ csrf_field() }}
-                    <p></p>
-
-                    <div class="form-group{{ $errors->has('filename') ? ' has-error' : '' }}">
-                        <label for="filename" class="col-md-2 control-label">Filename</label>
-
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" name="filename" value="{{ $movie->filename }}" required>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <div class="col-md-8 col-md-offset-4">
-                            <button type="submit" class="btn btn-primary">
-                                Submit
-                            </button>
-                        </div>
-                    </div>
-                </form>
-                </div>
-            </div>
+        @include('movies.partials.form')
 
         </div>
     </div>
