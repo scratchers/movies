70c270df764b7ad3202a948a1b2d5ef768633ad7
Jeff Puckett <jeff@jeffpuckett.com>
2017-02-19 04:11:19 +0000

    add group view show

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -46,7 +46,7 @@ class GroupController extends Controller
      */
     public function show(Group $group)
     {
-        //
+        return view('groups.show', ['group' => $group]);
     }
 
     /**
