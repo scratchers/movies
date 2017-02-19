beea5ab737db17a1de42c18e275c012c4a2bac0b
Jeff Puckett <jeff@jeffpuckett.com>
2017-02-19 04:00:46 +0000

    register group policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use App\Movie;
 use App\Policies\MoviePolicy;
+use App\Group;
+use App\Policies\GroupPolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -17,6 +19,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
         Movie::class => MoviePolicy::class,
+        Group::class => GroupPolicy::class,
     ];
 
     /**
