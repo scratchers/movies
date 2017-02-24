1fe85f10de168ec790b4b4708ac896218fc89358
Jeff Puckett <jeff@jeffpuckett.com>
2017-02-24 03:50:22 +0000

    add select2 stub for groups on movie edit form

--- resources/views/movies/edit.blade.php
+++ resources/views/movies/edit.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.app')
 
+@section('head')
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+<style>
+.flex-container {
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+}
+</style>
+@endsection()
+
+@section('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+
+<script type="text/javascript">
+$('select').select2();
+</script>
+@endsection()
+
 @section('content')
 <div class="container">
     <div class="row">
@@ -9,7 +28,14 @@
 
         @can('delete', $movie)
         <div class="panel panel-default">
-            <div class="panel-body">
+            <div class="panel-body flex-container">
+
+                <select class="js-example-basic-multiple" multiple="multiple" style="width: 50%">
+                    <option value="AL">Alabama</option>
+                    <option value="AR">Arkansas</option>
+                    <option value="WY">Wyoming</option>
+                </select>
+
                 <form role="form"
                     action="{{ route('movies.destroy', $movie) }}"
                     method="POST"
@@ -22,6 +48,7 @@
                         Delete
                     </button>
                 </form>
+
             </div>
         </div>
         @endcan
