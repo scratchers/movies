7c4c196b6ff09935e41542810296db5807e2bd90
Jeff Puckett <jeff@jeffpuckett.com>
2017-03-01 13:35:48 +0000

    add soft deletes

--- database/migrations/2017_02_19_162119_create_group_user_table.php
+++ database/migrations/2017_02_19_162119_create_group_user_table.php
@@ -14,6 +14,8 @@ class CreateGroupUserTable extends Migration
     public function up()
     {
         Schema::create('group_user', function (Blueprint $table) {
+            $table->softDeletes();
+
             $table->integer('group_id')->unsigned();
             $table->foreign('group_id')
                 ->references('id')
--- database/migrations/2017_02_19_203434_create_group_movie_table.php
+++ database/migrations/2017_02_19_203434_create_group_movie_table.php
@@ -14,6 +14,8 @@ class CreateGroupMovieTable extends Migration
     public function up()
     {
         Schema::create('group_movie', function (Blueprint $table) {
+            $table->softDeletes();
+
             $table->integer('group_id')->unsigned();
             $table->foreign('group_id')
                 ->references('id')
