52c43b2e6e1fc234645f882a8afc708b217cf399
Jeff Puckett <jeff@jeffpuckett.com>
2017-03-26 17:35:56 +0000

    implement omdb api request essentials

--- app/Meta/OmdbApi.php
+++ app/Meta/OmdbApi.php
@@ -3,14 +3,27 @@
 namespace App\Meta;
 
 use App\Movie;
+use InvalidArgumentException;
 
 class OmdbApi extends MetaService
 {
-    protected function initialize(){}
+    protected function initialize(){
+        $this->hostname = 'https://www.omdbapi.com';
+    }
 
-    protected function validate(){}
+    protected function validate(){
+        if ( empty($this->movie->imdb_id) && empty($this->movie->title) ) {
+            throw new InvalidArgumentException(
+                'OMDB API requires a title or an IMDB id.'
+            );
+        }
+    }
 
     protected function query() : string {
-        return '';
+        if ( !empty($this->movie->imdb_id) ) {
+            return "{$this->hostname}/?i={$this->movie->imdb_id}";
+        }
+
+        return "{$this->hostname}/?t={$this->movie->title}";
     }
 }
